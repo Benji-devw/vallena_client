1ce64967cb1d72d78d3c4935ac2fa364000cbf7c
Benji-devw <ben.nav@pm.me>
2025-04-27 16:35:02 +0200

    test/fix dependance

---
+++ src/types/vitest.d.ts
@@ -0,0 +1,6 @@
+import '@testing-library/jest-dom';
+
+declare module 'vitest' {
+  export interface Assertion<T = any> extends jest.Matchers<void, T> {}
+  export interface AsymmetricMatchersContaining extends jest.Matchers<void, any> {}
+} 
