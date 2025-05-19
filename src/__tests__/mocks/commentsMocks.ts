dcd414916b294199d1d4491ca06edfaa1e6f0384
Navart <ben.nav@pm.me>
2025-05-19 17:59:05 +0200

    fix/add mocks comments

--- src/__tests__/mocks/commentsMocks.ts
+++ src/__tests__/mocks/commentsMocks.ts
@@ -1,7 +1,18 @@
 import { Comment } from '@/types/commentTypes';
 
 export const mockComments: Comment[] = [
-
+  {
+    _id: '111',
+    orderNumber: '123456',
+    idProduct: '1',
+    by: 'John Doe',
+    messageTitle: 'Superbe robe',
+    message: 'Cette robe est magnifique et confortable. Je recommande fortement !',
+    note: 2,
+    dateBuy: '2024-01-15',
+    datePost: '2024-01-15',
+    status: true,
+  },
   {
     _id: '222',
     orderNumber: '123456',
